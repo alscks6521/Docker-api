6475d8ae9279595b5199ee27a2ff2adf09a6826b
MinQDU <2024513@daelim.ac.kr>
2024-04-30 23:16:43 +0900

    first commit

--- database/src/main/kotlin/com/daelim/database/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.daelim.database.repository
-
-import com.daelim.database.dto.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-}
