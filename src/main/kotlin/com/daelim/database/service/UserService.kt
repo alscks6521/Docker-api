a1860b45123a89f97333b6039132f67ada219e95
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-05-21 12:38:04 +0900

    Update UserService.kt

--- src/main/kotlin/com/daelim/database/service/UserService.kt
+++ src/main/kotlin/com/daelim/database/service/UserService.kt
@@ -3,7 +3,6 @@ package com.daelim.database.service
 import com.daelim.database.core.entity.UserEntity
 import com.daelim.database.repository.UserRepository
 import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
 
@@ -12,37 +11,33 @@ class UserService(
     private val userRepository: UserRepository,
     private val redisTemplate: StringRedisTemplate
 ) {
-    
-
     fun registerUser(username: String, password: String): UserEntity {
         // 비밀번호 길이 검증
         if (password.length < 8) {
             throw IllegalArgumentException("비밀번호는 8자리 이상 되어야 해요.")
         }
-
-
+        
         // 중복 사용자 확인
         val existingUser = userRepository.findByUsername(username)
         if (existingUser != null) {
             throw IllegalStateException("Username already exists")
         }
-
+        
         /**
          * 1. 상위 스코프에서 암호화 변수 선언 private val passwordEncoder = BCryptPasswordEncoder()
          * 비밀번호 단방향 해시 함수를 사용하여 암호화해야 합니다.
          * BCryptPasswordEncoder를 사용하여 다음과 같이 암호화할 수 있습니다.
          * 2. val encodedPassword = BCryptPasswordEncoder().encode(password)
          */
+        
         val user = UserEntity(username = username, password = password)
         return userRepository.save(user)
     }
 
-    
     fun validateUser(username: String, password: String): Boolean {
         val user = userRepository.findByUsername(username)
-
-        // 입력된 비밀번호와 데이터베이스에 저장된 해시 값을 비교하여 인증을 수행
-        return user?.let { passwordEncoder.matches(password, user.password) } ?: false
+        // 입력된 비밀번호와 데이터베이스에 저장된 비밀번호를 비교하여 인증을 수행
+        return user?.let { it.password == password } ?: false
     }
 
     fun createSession(username: String): String {
